ccf4fd7948ee55341f986a509c00d0d50e45fcaf
inkoromi21 <sema.vorobev20078@gmail.com>
2026-05-28 20:22:20 +0700

    Черновой каркас консольного меню

--- Pizza-Shop/Pizza-Shop/Program.cs
+++ Pizza-Shop/Pizza-Shop/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pizza_Shop
 {
@@ -10,6 +6,92 @@ namespace Pizza_Shop
   {
     static void Main(string[] args)
     {
+      Console.OutputEncoding = System.Text.Encoding.UTF8;
+      Console.Title = "Pizza Shop (черновик)";
+
+      while (true)
+      {
+        Console.WriteLine("=== PIZZA SHOP ===");
+        Console.WriteLine("Выберите действие:");
+        Console.WriteLine("1) Заказать пиццу");
+        Console.WriteLine("2) Показать меню");
+        Console.WriteLine("0) Выход");
+        Console.Write("Ваш выбор: ");
+
+        var action = Console.ReadLine();
+        Console.WriteLine();
+
+        if (action == "0")
+        {
+          Console.WriteLine("До свидания!");
+          return;
+        }
+
+        if (action == "2")
+        {
+          PrintMenu();
+          ContinuePrompt();
+          continue;
+        }
+
+        if (action != "1")
+        {
+          Console.WriteLine("Неизвестная команда. Попробуйте снова.");
+          Console.WriteLine();
+          continue;
+        }
+
+        PrintMenu();
+        Console.Write("Введите номер пиццы: ");
+        var pizzaChoice = Console.ReadLine();
+        Console.WriteLine();
+
+        switch (pizzaChoice)
+        {
+          case "1":
+            ProcessOrder("Маргарита");
+            break;
+          case "2":
+            ProcessOrder("Пепперони");
+            break;
+          case "3":
+            ProcessOrder("Гавайская");
+            break;
+          default:
+            Console.WriteLine("Такой пиццы нет в меню.");
+            break;
+        }
+
+        ContinuePrompt();
+      }
+    }
+
+    private static void PrintMenu()
+    {
+      Console.WriteLine("Меню:");
+      Console.WriteLine("1) Маргарита");
+      Console.WriteLine("2) Пепперони");
+      Console.WriteLine("3) Гавайская");
+      Console.WriteLine();
+    }
+
+    private static void ProcessOrder(string pizzaName)
+    {
+      Console.WriteLine($"Заказ принят: {pizzaName}");
+      Console.WriteLine("Готовим...");
+      Console.WriteLine($"- Подготовка ингредиентов для \"{pizzaName}\"");
+      Console.WriteLine($"- Выпекаем \"{pizzaName}\"");
+      Console.WriteLine($"- Нарезаем \"{pizzaName}\"");
+      Console.WriteLine($"- Упаковываем \"{pizzaName}\"");
+      Console.WriteLine("Готово! Приятного аппетита.");
+      Console.WriteLine();
+    }
+
+    private static void ContinuePrompt()
+    {
+      Console.Write("Нажмите Enter, чтобы продолжить...");
+      Console.ReadLine();
+      Console.WriteLine();
     }
   }
 }
